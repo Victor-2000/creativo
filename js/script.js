e0fc9d3bc47df8639a92f26dff074825abd32c0e
Victor <17victor.c@gmail.com>
2020-08-28 00:02:22 +0300

    Made blog_detail.html and about.html mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,45 +1,72 @@
-(function () {
-  contentButtonPairs = {
-    whoContent: { content: $(".content.who"), button: $(".button.who") },
-    whatContent: { content: $(".content.what"), button: $(".button.what") },
-    whyContent: { content: $(".content.why"), button: $(".button.why") },
-  };
+sidebar = $(".sidebar");
+hamburger_sidebar = $(".side-menu-hamburger-button");
 
-  maxHeight =
+contentButtonPairs = {
+  whoContent: { content: $(".content.who"), button: $(".button.who") },
+  whatContent: { content: $(".content.what"), button: $(".button.what") },
+  whyContent: { content: $(".content.why"), button: $(".button.why") },
+};
+
+maxHeight =
+  Math.max(
+    contentButtonPairs.whoContent.content.height(),
     Math.max(
-      contentButtonPairs.whoContent.content.height(),
-      Math.max(
-        contentButtonPairs.whatContent.content.height(),
-        contentButtonPairs.whyContent.content.height()
-      )
-    ) + 20;
+      contentButtonPairs.whatContent.content.height(),
+      contentButtonPairs.whyContent.content.height()
+    )
+  ) + 20;
 
-  console.log(maxHeight);
+console.log(maxHeight);
 
-  contentButtonPairs.whoContent.content.height(maxHeight);
-  contentButtonPairs.whatContent.content.height(maxHeight);
-  contentButtonPairs.whyContent.content.height(maxHeight);
+switch (maxHeight - 20) {
+  case contentButtonPairs.whoContent.content.height():
+    console.log("Who");
+    break;
+  case contentButtonPairs.whatContent.content.height():
+    console.log("What");
+    break;
+  case contentButtonPairs.whyContent.content.height():
+    console.log("Why");
+    break;
+  default:
+    break;
+}
 
-  last = "whoContent";
+contentButtonPairs.whoContent.content.height(maxHeight);
+contentButtonPairs.whatContent.content.height(maxHeight).css("display", "none");
+contentButtonPairs.whyContent.content.height(maxHeight).css("display", "none");
 
-  $.each(contentButtonPairs, function (key, currentPair) {
-    currentPair.button.click(() => {
-      changeContent(key, currentPair.content, currentPair.button);
-    });
+last = "whoContent";
+
+$.each(contentButtonPairs, function (key, currentPair) {
+  currentPair.button.click(() => {
+    changeContent(key, currentPair.content, currentPair.button);
   });
+});
 
-  function changeContent(key, currentContent, currentButton) {
-    console.log(currentContent);
-    if (last !== currentContent) {
-      //content change
-      contentButtonPairs[last].content.fadeOut(400, function () {
-        //buttons change
-        contentButtonPairs[last].button.removeClass("selected");
-        currentButton.addClass("selected");
-        currentContent.fadeIn(400, function () {
-          last = key;
-        });
+function changeContent(key, currentContent, currentButton) {
+  console.log(currentContent);
+  if (last !== currentContent) {
+    //content change
+    contentButtonPairs[last].content.fadeOut(400, function () {
+      //buttons change
+      contentButtonPairs[last].button.removeClass("selected");
+      currentButton.addClass("selected");
+      currentContent.fadeIn(400, function () {
+        last = key;
       });
-    }
+    });
   }
-})();
+}
+
+function closeSidebar() {
+  sidebar_width = sidebar.width();
+
+  sidebar.css("right", -sidebar_width - 30);
+  hamburger_sidebar.css("right", -sidebar_width - 30);
+}
+
+function openSidebar() {
+  sidebar.css("right", "-10px");
+  hamburger_sidebar.css("right", "-10px");
+}
